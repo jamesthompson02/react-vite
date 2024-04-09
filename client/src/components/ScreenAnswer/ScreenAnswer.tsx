6941346d4ec191f9100a385cc2aa6f6a30a9fb8b
jamesthompson02 <jamesthompsongithub@outlook.com>
2024-04-09 11:20:49 +0100

    attempt at reducing font size  where numbers in calculator screen get too long using hooks.

--- client/src/components/ScreenAnswer/ScreenAnswer.tsx
+++ client/src/components/ScreenAnswer/ScreenAnswer.tsx
@@ -1,14 +1,49 @@
 import "./ScreenAnswer.css";
 import { useAppSelector } from "../../hooks/storeHooks";
+import { useEffect, useRef, useState } from "react";
 
 const ScreenAnswer = () => {
+  // selectors
   const firstNumber = useAppSelector((state) => state.calculator.firstNumber);
   const secondNumber = useAppSelector((state) => state.calculator.secondNumber);
   const result = useAppSelector((state) => state.calculator.result);
   const operator = useAppSelector((state) => state.calculator.operator);
+
+  // hooks
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [fontSize, setFontSize] = useState<number>(32);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    const adjustFontSize = () => {
+      const { scrollWidth, clientWidth } = container;
+      console.log(scrollWidth, clientWidth);
+      const ratio = clientWidth / scrollWidth;
+
+      if (ratio <= 1) {
+        setTimeout(
+          () => setFontSize((prevFontSize) => prevFontSize * ratio),
+          200
+        );
+      } else {
+        setTimeout(
+          () => setFontSize((prevFontSize) => prevFontSize * (1 + (1 - ratio))),
+          200
+        );
+      }
+    };
+
+    adjustFontSize();
+  }, [firstNumber, secondNumber, result, operator]);
   return (
     <>
-      <div className="screen-answer">
+      <div
+        ref={containerRef}
+        style={{ fontSize: `${fontSize}px`, overflow: "hidden" }}
+        className="screen-answer"
+      >
         {firstNumber &&
           operator &&
           secondNumber &&
